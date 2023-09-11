b3cf29d615c3679e4ceecf640e5341e34955c8ea
RafaelaFoerster <rafafoerst@gmail.com>
2023-09-11 18:41:52 -0300

    listar ingressos test

--- pages/usuario/listaringressos.vue
+++ pages/usuario/listaringressos.vue
@@ -1,47 +1,30 @@
 <template>
-    <v-table fixed-header height="300px">
-      <thead>
-        <tr>
-          <th class="text-left">
-            Filme
-          </th>
-          <th class="text-left">
-            Sessão
-          </th>
-          <th class="text-left">
-            Vaga
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="ingresso in ingressos" :key="ingresso.id" >
-          <td>{{ ingresso.filme }}</td>
-          <td>{{ ingresso.sessao }}</td>
-          <td>{{ ingresso.vagas }}</td>
-        </tr>
-      </tbody>
-    </v-table>
-    <RouterLink to="compraringresso">Comprar Novo</RouterLink>
+  <v-main>
+    <v-container fluid>
+      <h1 class="headline">Ingressos</h1>
+      <lista-ingressos></lista-ingressos> 
+    </v-container>
+  </v-main>
 </template>
-<script setup>
-    import IngressoService from '@/service/IngressoService';
-    var ingressos = ref([
-        {
-            "filme": "barbie",
-            "vagas":[8],
-	        "sessao": "27/09"
-        }
-    ]);
 
-    function listarIngressos () {
-        IngressoService.list().then(
-            response => {
-                ingressos.value = response.data;
-            }
-        )
-    }
+<script>
 
-    onMounted(
-        () => {listarIngressos();} 
-    )
+definePageMeta({
+  layout: 'usuario'
+})
+import listarIngressos from "@/components/ListarIngressos.vue";
+
+export default {
+  name: "Home",
+  components: {
+    ListarIngressos,
+  },
+};
 </script>
+
+<style scoped>
+.headline {
+  margin-top: 2rem;
+  text-align: center;
+}
+</style>
